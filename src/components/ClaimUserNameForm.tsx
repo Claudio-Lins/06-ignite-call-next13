497990fe26443d45f39bfb8c7171317ea927bf99
Claudio Lins <claudio.lins@me.com>
2023-08-31 15:58:56 +0100

    butto and input reservar

--- src/components/ClaimUserNameForm.tsx
+++ src/components/ClaimUserNameForm.tsx
@@ -1,13 +1,13 @@
-"use client";
+"use client"
 
-import React from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import { ArrowRight } from "phosphor-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
+import React from "react"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+import { ArrowRight } from "phosphor-react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation"
 
 const claimUsernameFormSchema = z.object({
   username: z
@@ -22,12 +22,12 @@ const claimUsernameFormSchema = z.object({
       message: "O nome de usuário deve conter apenas letras e traços",
     })
     .transform((username) => username.toLowerCase()),
-});
+})
 
-type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUserNameForm() {
-  const router = useRouter();
+  const router = useRouter()
   const {
     register,
     handleSubmit,
@@ -35,12 +35,12 @@ export function ClaimUserNameForm() {
     formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
-  });
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
-    const { username } = data;
-    await router.push(`/register?username=${username}`);
-    reset();
+    const { username } = data
+    await router.push(`/register?username=${username}`)
+    reset()
   }
 
   return (
@@ -48,22 +48,33 @@ export function ClaimUserNameForm() {
       onSubmit={handleSubmit(handleClaimUsername)}
       className="flex flex-col bg-gray-800 p-4 rounded-lg"
     >
-      <div className="flex items-center gap-2 mb-2">
-        <span>clins.com/</span>
-        <Input
-          type="text"
-          placeholder="Seu usuário"
-          {...register("username")}
-          className="
-          bg-gray-800
+      <div className="flex items-center justify-between gap-4">
+        <div className="flex items-center gap-0 mb-2 border p-2 rounded-lg flex-1">
+          <span>clins.com/</span>
+          <Input
+            type="text"
+            placeholder="Seu usuário"
+            {...register("username")}
+            className="
+          w-1/2
+          -ml-2
+          border-0
+          bg-transparent
           text-white
           placeholder-gray-400
+          focus:placeholder-gray-500
           focus:ring-0
-          focus:outline-none
+          focus:ring-transparent
+          focus:border-black
         "
-        />
-        <Button type="submit" disabled={isSubmitting}>
-          Resevar usuário
+          />
+        </div>
+        <Button
+          className=" bg-ignite-500"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Resevar
           <ArrowRight />
         </Button>
       </div>
@@ -72,5 +83,5 @@ export function ClaimUserNameForm() {
         <small className="text-red-200">{errors.username.message}</small>
       )}
     </form>
-  );
+  )
 }
